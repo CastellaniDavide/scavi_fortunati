d8a675de224a3c5137ff53cc74c2b6aff721c79e
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-02 15:50:31 +0200

    Created bin/template.cpp

---
+++ bin/scavi_fortunati.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file scavi_fortunati.cpp
+ *
+ * @version 01.01 2020-9-2
+ *
+ * @brief https://training.olinfo.it/#/task/tai_scavi/statement
+ *
+ * @ingroup scavi_fortunati
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+//  uncomment the following lines if you want to read/write from files
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  cin >> N;
+
+  return 0;
+}
